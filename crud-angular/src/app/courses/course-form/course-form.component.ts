c5c6eb238bd551acdd5b7562ce90f16fc6420660
alanaafsc <alana.ingrid@ufpe.br>
2023-01-19 22:13:10 -0300

    Angular Typed Forms

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { Location } from '@angular/common';
+import { Component } from '@angular/core';
+import { NonNullableFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { CoursesService } from '../services/courses.service';
 
@@ -11,16 +11,16 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CourseFormComponent {
 
-  form: UntypedFormGroup;
+  form = this.formBuilder.group({
+    name: [''],
+    category: ['']
+  });
 
-  constructor(private formBuilder: UntypedFormBuilder,
+  constructor(private formBuilder: NonNullableFormBuilder,
     private service: CoursesService,
     private snackBar: MatSnackBar,
-    private location: Location){
-    this.form = this.formBuilder.group({
-      name: [null],
-      category: [null]
-    });
+    private location: Location){ 
+      //this.form
   }
 
   onSubmit() {
